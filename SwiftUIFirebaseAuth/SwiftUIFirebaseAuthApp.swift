68df94b533705e03aa5da89199d14079f011b110
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-08 16:46:01 +0200

    Initial Commit

---
+++ SwiftUIFirebaseAuth/SwiftUIFirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseAuthApp.swift
+//  SwiftUIFirebaseAuth
+//
+//  Created by Ndayisenga Jean Claude on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
